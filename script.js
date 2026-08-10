919e6357c7e3b61492a1c2eab17fc2313c7a4642
ikio-nen <148907316+ikio-nen@users.noreply.github.com>
2026-08-11 03:30:55 +0530

    pae2 done

--- script.js
+++ script.js
@@ -47,16 +47,36 @@ function navAnimation() {
         }, "-=0.2");
     });
 } 
+ navAnimation();
 
-// navAnimation();
-
+function page2animate (){
+    
 var rightElems = document.querySelectorAll(".right-elem")
 
 rightElems.forEach(function(elem){
+    
     elem.addEventListener("mouseenter",function(){
-        elem.childNodes[3].style.opacity = 1
+        gsap.to(elem.childNodes[3],{
+            opacity: 0.8,
+            scale: 1 
+        })
+    })
+
+    elem.addEventListener("mouseleave",function(){
+        gsap.to(elem.childNodes[3],{
+            opacity: 0,
+            scale: 0
+        })
+    })
+    
+    
+    elem.addEventListener("mousemove",function(dets){ 
+        gsap.to(elem.childNodes[3],{
+            x: dets.x - elem.getBoundingClientRect().x - 50,
+            y: dets.y - elem.getBoundingClientRect().y - 150
+        })
+    })
 })
- elem.addEventListener("mouseleave",function(){
-        elem.childNodes[3].style.opacity = 0
-})
-})
+}
+
+page2animate();
